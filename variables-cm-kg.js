3f047383c668dc1cc616ba17ff765d0a987523e2
Hugo Romero <hurodev2@gmail.com>
2022-08-03 15:02:15 -0500

    variables numbers y limites

---
+++ variables-cm-kg.js
@@ -0,0 +1,13 @@
+const { Value } = require("sass");
+
+let miAltura = 169;
+let miAlturaM = 1.69;
+let miPeso = 65.5;
+let miPesoRendon = miPeso.toFixed();
+let maxJav = Number.MAX_VALUE;
+let x = maxJav + 1
+
+
+console.log(x);
+
+
